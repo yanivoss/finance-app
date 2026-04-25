97e9041d4b24c54f67384d5d58e730639108cfe3
yanivoss <69387025+yanivoss@users.noreply.github.com>
2026-04-25 23:20:28 +0300

    Refactor live value retrieval and update logic

--- streamlit_app.py
+++ streamlit_app.py
@@ -216,12 +216,15 @@ st.markdown("""
 try:
     df_s = pd.read_csv(URL_SUMMARY)
     # 1. הרצת חישוב הלייב (אם לא הורץ קודם)
-live_issta_value, current_issta_price = get_issta_live_value()
-
-if live_issta_value is not None:
-    # 2. עדכון הערך בתוך df_s בשורה הרלוונטית (שורה 4, עמודה 3 - אינדקס 2)
-    # אנחנו מעדכנים ישירות את התא שממנו i_n שואב את המידע
-    df_s.iloc[3, 2] = live_issta_value
+    live_issta_value, current_issta_price = get_issta_live_value()
+    
+    if live_issta_value is not None:
+        # 2. עדכון הערך בתוך df_s בשורה הרלוונטית (שורה 4, עמודה 3 - אינדקס 2)
+        # אנחנו מעדכנים ישירות את התא שממנו i_n שואב את המידע
+        df_s.iloc[3, 2] = live_issta_value
+        
+    # 3. שמירת מחיר המניה לשימוש בטאבים
+    st.session_state['last_issta_price'] = current_issta_price
     
     # 3. שמירת מחיר המניה לשימוש בטאבים
     st.session_state['last_issta_price'] = current_issta_price
